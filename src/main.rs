708a3946c1e3562731a40c4af08272b6fb37c9f2
David Carlton <carlton@sumologic.com>
2015-08-19 10:06:04 -0700

    Initial request to the endpoint, with username and password.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate hyper;
+extern crate rpassword;
+
+use hyper::client::Client;
+use hyper::header::{Headers, Authorization, Basic};
+use rpassword::read_password;
+use std::env;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let endpoint = "https://api.sumologic.com/api/v1/search/jobs";
+    let args: Vec<_> = env::args().collect();
+    let ref username = args[1];
+    print!("Password: ");
+    io::stdout().flush().unwrap();
+    let password = read_password().unwrap();
+
+    let client = Client::new();
+
+    let mut headers = Headers::new();
+    headers.set(
+        Authorization(
+            Basic {
+                username: username.to_owned(),
+                password: Some(password.to_owned())
+            }
+            )
+            );
+
+    let res = client.post(endpoint)
+        .headers(headers)
+        .send()
+        .unwrap();
+
+    println!("{}", res.status);
 }
